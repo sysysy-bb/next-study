47c2b5c713249f30340200f7810cfd4be27059a8
sysysy-bb <19113115916@163.com>
2025-08-06 16:16:45 +0800

    平行路由

--- src/app/@analytics/page.js
+++ src/app/@analytics/page.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>about</div>
+    <div className='analytics'>@analytics</div>
+
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div>hello world</div>
+    <div className='children'>@children</div>
   )
 }
